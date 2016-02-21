9aaa1e3dabdd84c9443fb780e8299b7f68439672
Francis Lawlor <franlawlor0@gmail.com>
2016-02-21 22:47:13 +0000

    Added Die.class

---
+++ Risk/src/Dice/Die.java
@@ -0,0 +1,20 @@
+package Dice;
+
+public class Die {
+	private int diceFace;
+	
+	//constructor
+	public Die(){
+		roll();
+	}
+	
+	//assign random number between 1 and 6 to diceFace
+	public void roll(){
+		diceFace = (int)(Math.random() * 6);
+	}
+	
+	//return diceFace
+	public int getFace(){
+		return diceFace;
+	}
+}
