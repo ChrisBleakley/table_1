1b4aad9eafb2f4185606ba1fa7ca5eb69f874dc9
Francis Lawlor <franlawlor0@gmail.com>
2016-03-27 06:42:04 +0100

    Replaced background colour with map image.

--- Risk/src/GUI/MapPanel.java
+++ Risk/src/GUI/MapPanel.java
@@ -10,8 +10,13 @@ package GUI;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 public class MapPanel extends JPanel {
@@ -24,11 +29,41 @@ public class MapPanel extends JPanel {
 	}
 	@Override
 	public void paintComponent(Graphics g){
+		//get image locally
+		try {
+			image = ImageIO.read(new File("MapImages/worldmap.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		//FRANCIS TODO CHANGE CO-ORDINATES ON MAP
 		super.paintComponent(g);
+
+//		DON'T REMOVE UNTIL FRANCIS IS FINISHED YO
+//		//open image from url
+//		URL url = null;
+//		try {
+//			url = new URL("http://wallpapercave.com/wp/HAJPo5c.png");
+//		} catch (MalformedURLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		
+//		try {
+//			image = ImageIO.read(url);
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		
 		Graphics2D gfx2d = (Graphics2D)g;
-		gfx2d.setColor(new Color(0,191,255));
 		gfx2d.fill(new Rectangle(0, 0, (int)output.getPanelSize().getWidth(), (int)output.getPanelSize().getHeight()));	
+		Image scaled = image.getScaledInstance(this.getWidth(),this.getHeight(),Image.SCALE_SMOOTH);
+		g.drawImage(scaled, 0, 0, null);
+		gfx2d.setColor(new Color(0,191,255));
 	}
 	private Output output;
 	private static final long serialVersionUID = 1L;
+	private BufferedImage image;
 }
