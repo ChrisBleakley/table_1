67370cff3ce5759842a153c9ca9962c7ee69e89e
Francis Lawlor <franlawlor0@gmail.com>
2016-04-10 16:06:40 +0100

    Fixed issues with map display

--- Risk/src/GUI/MapPanel.java
+++ Risk/src/GUI/MapPanel.java
@@ -10,12 +10,18 @@ package GUI;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
 
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
 //import javax.swing.JFrame;
 import javax.swing.JPanel;
 //import javax.swing.SwingUtilities;
+import javax.swing.SwingUtilities;
 
 public class MapPanel extends JPanel {
 	public MapPanel(Output output){
@@ -28,38 +34,22 @@ public class MapPanel extends JPanel {
 	@Override
 	public void paintComponent(Graphics g){
 		//get image locally
-//		try {
-//			image = ImageIO.read(new File("MapImages/worldmap.png"));
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-
-		//FRANCIS TODO CHANGE CO-ORDINATES ON MAP
-		super.paintComponent(g);
-
-//		DON'T REMOVE UNTIL FRANCIS IS FINISHED YO
-//		//open image from url
-//		URL url = null;
-//		try {
-//			url = new URL("http://wallpapercave.com/wp/HAJPo5c.png");
-//		} catch (MalformedURLException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		
-//		try {
-//			image = ImageIO.read(url);
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+		URL url = MapPanel.class.getResource("/worldmap.png");
 		
-		//JFrame topFrame = (JFrame) SwingUtilities.getWindowAncestor(this);
+		try {
+			image = ImageIO.read(url.openStream());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		super.paintComponent(g);
+		
+		JFrame topFrame = (JFrame) SwingUtilities.getWindowAncestor(this);
 		Graphics2D gfx2d = (Graphics2D)g;
 		gfx2d.fill(new Rectangle(0, 0, (int)output.getPanelSize().getWidth()+60, (int)output.getPanelSize().getHeight()));	
-		//Image scaled = image.getScaledInstance(this.getWidth(),this.getHeight(),Image.SCALE_SMOOTH);
-		//g.drawImage(scaled, 0, 0, null);
+		Image scaled = image.getScaledInstance(this.getWidth(),this.getHeight(),Image.SCALE_SMOOTH);
+		g.drawImage(scaled, 0, 0, null);
 		gfx2d.setColor(new Color(0,191,255));
 	}
 	private Output output;
